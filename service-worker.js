e2f7bd4a4786fe9a1d38cf27d35e9738b0ee3a3d
Landon James <landonjames@Landon-Mac-2023.local>
2026-06-16 21:37:23 -0700

    Release v48

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
  * open page to reload onto the new version.
  */
 
-const CACHE_VERSION = 'v47';
+const CACHE_VERSION = 'v48';
 const CACHE = `cinematracker-${CACHE_VERSION}`;
 
 self.addEventListener('install', () => {
