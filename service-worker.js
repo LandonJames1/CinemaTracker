23abd5e28b9b3ea595f703441810bd29ebdc2dc8
Landon James <landon.talus@gmail.com>
2026-07-16 09:52:30 -0700

    Release v135

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
  * open page to reload onto the new version.
  */
 
-const CACHE_VERSION = 'v134';
+const CACHE_VERSION = 'v135';
 const CACHE = `cinematracker-${CACHE_VERSION}`;
 
 self.addEventListener('install', () => {
