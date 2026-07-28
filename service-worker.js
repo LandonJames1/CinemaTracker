ee7a53081a0ae3d49899aa51b975d255e935d488
Landon James <landon.talus@gmail.com>
2026-07-27 17:43:24 -0700

    Release v143

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
  * open page to reload onto the new version.
  */
 
-const CACHE_VERSION = 'v142';
+const CACHE_VERSION = 'v143';
 const CACHE = `cinematracker-${CACHE_VERSION}`;
 
 self.addEventListener('install', () => {
